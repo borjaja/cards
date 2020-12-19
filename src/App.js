13308265dcae0f90e639d1b39d79f7a29f2c4781
Borja <borja.alvarez.pineiro@udc.es>
2020-12-19 18:33:08 +0100

    More components and using faker.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react'
 import './App.css';
 import Card from './Card'
+import faker from 'faker'
+
 function App() {
   return (
     <div className="App">
-      <Card name="Alan smith" />
-      <Card name="Alan smith" />
-      <Card name="Alan smith" />
+      <Card
+        name={` ${faker.name.firstName()} ${faker.name.lastName()}`}
+        title={faker.name.jobTitle()}
+        avatar={faker.image.city()} />
+      <Card
+        name={` ${faker.name.firstName()} ${faker.name.lastName()}`}
+        title={faker.name.jobTitle()}
+        avatar={faker.image.fashion()} />
+      <Card
+        name={` ${faker.name.firstName()} ${faker.name.lastName()}`}
+        title={faker.name.jobTitle()}
+        avatar={faker.image.nightlife()} />
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -4,10 +4,10 @@ import './App.css'
 const Card = (props) => {
     return (
         <div className="card">
-            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{ width: '100%' }} />
+            <img src={props.avatar} alt="Avatar" style={{ width: '100%' }} />
             <div className="container">
                 <h4><b>{props.name}</b></h4>
-                <p>Architect & Engineer</p>
+                <p>{props.title}</p>
             </div>
         </div>
     )
