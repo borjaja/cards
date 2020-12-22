77501af7f4b34a5730829b30b57927a7cf69828a
Borja Alvarez <balvarez@orienteed.com>
2020-12-22 10:36:50 +0100

    Class components and refactor

--- src/App.js
+++ src/App.js
@@ -1,86 +1,81 @@
-import React, {Component} from 'react'
-import './App.css';
-import Card from './Card'
-import faker from 'faker'
-import {ThemeProvider} from 'styled-components'
-import Button from './element/Button'
+import React, {Component} from "react";
+import "./App.css";
+import Card from "./Card";
+import faker from "faker";
+import {ThemeProvider} from "styled-components";
+import Button from "./element/Button";
 
 const theme = {
-  primary: '#4caf50',
-  mango: 'yellow'
-}
+    primary: "#4caf50",
+    mango: "yellow",
+};
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      cards:[
-        {
-          id: 'aaaaaaaaaaaaaa',
-          name: faker.name.firstName(),
-          title: faker.name.jobTitle(),
-          avatar: faker.image.business(),
-        },
-        {
-          id: 'bbbbbbbbbbbbbb',
-          name: faker.name.firstName(),
-          title: faker.name.jobTitle(),
-          avatar: faker.image.abstract(),
-        },
-        {
-          id: 'ccccccccccccccc',
-          name: faker.name.firstName(),
-          title: faker.name.jobTitle(),
-          avatar: faker.image.people(),
-        },
-      ],
-    showCard:true
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: [
+                {
+                    id: "aaaaaaaaaaaaaa",
+                    name: faker.name.firstName(),
+                    title: faker.name.jobTitle(),
+                    avatar: faker.image.business(),
+                },
+                {
+                    id: "bbbbbbbbbbbbbb",
+                    name: faker.name.firstName(),
+                    title: faker.name.jobTitle(),
+                    avatar: faker.image.abstract(),
+                },
+                {
+                    id: "ccccccccccccccc",
+                    name: faker.name.firstName(),
+                    title: faker.name.jobTitle(),
+                    avatar: faker.image.people(),
+                },
+            ],
+            showCard: true,
+        };
     }
-  }
 
-  toggleVisible = () => this.setState({showCard: !this.state.showCard})
-  deleteCardHandler = (cardIndex) => {
-    const cards = [...this.state.cards]
-    cards.splice(cardIndex, 1)
-    this.setState({cards})
-  }
-  changeNameHandler = (event, id) => {
-    const cardIndex = this.state.cards.findIndex(card => card.id === id)
-    const cards = [...this.state.cards]
-    cards[cardIndex].name = event.target.value
-    this.setState({cards})
-  }
+    toggleVisible = () => this.setState({showCard: !this.state.showCard});
+    deleteCardHandler = (cardIndex) => {
+        const cards = [...this.state.cards];
+        cards.splice(cardIndex, 1);
+        this.setState({cards});
+    };
+    changeNameHandler = (event, id) => {
+        const cardIndex = this.state.cards.findIndex((card) => card.id === id);
+        const cards = [...this.state.cards];
+        cards[cardIndex].name = event.target.value;
+        this.setState({cards});
+    };
 
-  buttonStyle = {
-    backgroundColor: null
-  }
+    buttonStyle = {
+        backgroundColor: null,
+    };
 
-  render(){
-    const classes = ['button']
-    if (this.state.cards.length < 3) classes.push('pink')
-    if (this.state.cards.length < 2) classes.push('red')
+    render() {
+        const classes = ["button"];
+        if (this.state.cards.length < 3) classes.push("pink");
+        if (this.state.cards.length < 2) classes.push("red");
 
-    const cardsMarkup = this.state.showCard && this.state.cards.map(card =>
-      <Card
-        key={card.id}
-        name={card.name}
-        title={card.title}
-        avatar={card.avatar}
-        onChange={(event) => this.changeNameHandler(event, card.id)}
-        deleteCardHandler={() => this.deleteCardHandler(card.id)} />
-    )
+        const cardsMarkup = this.state.showCard && this.state.cards.map((card) => <Card key={card.id} name={card.name} title={card.title} avatar={card.avatar} onChange={(event) => this.changeNameHandler(event, card.id)} deleteCardHandler={() => this.deleteCardHandler(card.id)} />);
 
-    return (
-      <ThemeProvider theme={theme}>
-      <div className="App">
-        <Button color="mango" length={this.state.cards.length} onClick={this.toggleVisible}>Toggle</Button>
-        <button className={classes.join(' ')} onClick={this.toggleVisible}>Toggle</button>
-        {cardsMarkup}
-      </div>
-      </ThemeProvider>
-    );
-  }
+        return (
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    <Button color="mango" length={this.state.cards.length} onClick={this.toggleVisible}>
+                        Toggle
+                    </Button>
+                    <button className={classes.join(" ")} onClick={this.toggleVisible}>
+                        Toggle
+                    </button>
+                    {cardsMarkup}
+                </div>
+            </ThemeProvider>
+        );
+    }
 }
-
 
 export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import './App.css'
+import React, {Component} from "react";
+import "./App.css";
 
-const Card = (props) => {
-    return (
-        <div className="card">
-            <img src={props.avatar} alt="Avatar" style={{ width: '100%' }} />
-            <div className="container">
-                <h4><b>{props.name}</b></h4>
-                <p>{props.title}</p>
-                <input type="text" onChange={props.onChange} value={props.name} />
-                <button className="button button-red" onClick={props.deleteCardHandler}>Delete</button>
-                <div>{props.children}</div>
+class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                <img src={this.props.avatar} alt="Avatar" style={{width: "100%"}} />
+                <div className="container">
+                    <h4>
+                        <b>{this.props.name}</b>
+                    </h4>
+                    <p>{this.props.title}</p>
+                    <input type="text" onChange={this.props.onChange} value={this.props.name} />
+                    <button className="button button-red" onClick={this.props.deleteCardHandler}>
+                        Delete
+                    </button>
+                    <div>{this.props.children}</div>
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
-export default Card
+export default Card;
