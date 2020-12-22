cb51ee1f4c69584b75c86d3204dcf06c6732b27d
Borja <borja.alvarez.pineiro@udc.es>
2020-12-22 22:11:05 +0100

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useState, useMemo } from "react";
 import "./App.css";
-import axios from "axios";
-import { ListGroupItem, ListGroup, Badge } from 'reactstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const initialState = {
-    loading: true,
-    error: false,
-    todos: [],
-};
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'SET_DATA': return {
-            loading: false,
-            error: false,
-            todos: action.payload
-        }
-        case 'SET_ERROR': return {
-            loading: false,
-            error: true,
-            todos: []
-        }
-        default: return state
-    }
-}
+import Component1 from "./components/Component1";
 
 function App() {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/todos').then(res => {
-            console.log(res.data);
-            dispatch({ type: 'SET_DATA', payload: res.data })
-        }).catch(err => {
-            dispatch({ type: 'SET_ERROR' })
-        })
-    }, [])
-    const listMarkup =
-        <ListGroup>
-            {state.todos.map(todo => <ListGroupItem key={todo.id}>{todo.title} {todo.completed ? <Badge color="success">Completed</Badge> : <Badge color="danger">Incomplete</Badge>} </ListGroupItem>)}
-        </ListGroup>
+    const [countA, setCountA] = useState(0);
+    const incremetAddingOne = () => setCountA(countA + 1)
+    const memoComponentA = useMemo(() => <Component1 />, [])
     return (
         <div className="App">
-            {state.loading ? 'Loading...' : state.error ? 'error' : listMarkup}
-
+            App js count A: {countA}
+            <p>
+                <button onClick={incremetAddingOne}>Increment A
+                </button>
+            </p>
+            {memoComponentA}
         </div>
     );
 }
