bcd64ee672b455e6874d074dea62f32cc1544c1f
Borja Alvarez <balvarez@orienteed.com>
2020-12-23 13:56:00 +0100

    Custom hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React, {useState, useCallback} from "react";
+import React, {useRef, useState} from "react";
 import "./App.css";
-
-const functionCounter = new Set();
+import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
 function App() {
-    const [count1, setCount1] = useState(0);
-    const [count2, setCount2] = useState(0);
-    const incrementCount1 = useCallback(() => setCount1(count1 + 1), [count1]);
-    const incrementCount2 = useCallback(() => setCount2(count2 + 1), [count2]);
-    functionCounter.add(incrementCount1);
-    functionCounter.add(incrementCount2);
-    console.log(functionCounter);
     return (
-        <>
-            <h3>
-                Component count1: {count1} count2: {count2}
-            </h3>
-            <button onClick={incrementCount1}>Button1</button>
-            <button onClick={incrementCount2}>Button1</button>
-        </>
+        <div className="App">
+            <ComponentA />
+            <p></p>
+            <ComponentB />
+        </div>
     );
 }
 
