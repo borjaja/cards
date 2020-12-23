f948ac4184668f5f0c89ec441ada1175fdc0fb56
Borja Alvarez <balvarez@orienteed.com>
2020-12-23 11:41:19 +0100

    useRef

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React, {useState, useMemo} from "react";
+import React, {useRef} from "react";
 import "./App.css";
-import Component1 from "./components/Component1";
-import Component2 from "./components/Component2";
 
 function App() {
-    const [countA, setCountA] = useState(0);
-    const [countB, setCountB] = useState(0);
-    const incremetAAddingOne = () => setCountA(countA + 1);
-    const incremetBAddingOne = () => setCountB(countB + 1);
-    const memoComponentA = useMemo(() => <Component1 count={countA} />, [countA]);
+    const inputRef = useRef();
     return (
         <div className="App">
-            App js count A: {countA}
-            <p>
-                <button onClick={incremetAAddingOne}>Increment A</button>
-            </p>
-            <p>
-                <button onClick={incremetBAddingOne}>Increment B</button>
-            </p>
-            {memoComponentA}
-            <p></p>
-            <Component2 count={countB} />
+            <input ref={inputRef} type="text" />
+            <input type="text" />
+            <input type="text" />
+            <button onClick={() => inputRef.current.focus()}>Focus</button>
         </div>
     );
 }
