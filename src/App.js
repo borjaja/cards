01c907b86aa0878c0fe817e26fb91b2ff63aacda
Borja Alvarez <balvarez@orienteed.com>
2020-12-23 16:46:27 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./App.css";
-import Form from "./components/Form";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <Form />
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Route exact path="/" component={Home} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/about" component={About} />
+            </div>
+        </Router>
     );
 }
 
