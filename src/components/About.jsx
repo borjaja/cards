be83e71be775ebf8dae27aba71a7915e300b4b55
Borja <borja.alvarez.pineiro@udc.es>
2020-12-23 19:10:59 +0100

    high order components hoc

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import faker from "faker";
+import ColorFul from "../hoc/ColorFul";
 const About = () => {
     return (
         <div className="container">
@@ -9,4 +10,4 @@ const About = () => {
     );
 };
 
-export default About;
+export default ColorFul(About);
---
+++ src/hoc/ColorFul.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ColorFul = WrappedComponnet => {
+    const colors = [
+        "text - primary",
+        "text-secondary",
+        "text-success",
+        "text-danger",
+        "text-warning",
+        "text-info",
+        "text-light",
+        "text-dark",
+        "text-muted",
+        "text-white"
+    ]
+    const color = colors[Math.floor(Math.random() * 5)]
+    return (props) => {
+        return (
+            < div className={color}><WrappedComponnet {...props} /></div >
+        )
+    }
+}
+
+ColorFul.propTypes = {
+
+}
+
+export default ColorFul
