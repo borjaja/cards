f76303d644075f17f4440198ac124c758a65ffac
Borja Alvarez <balvarez@orienteed.com>
2020-12-24 11:55:00 +0100

    display value with useSelector and also useDispatch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import {useSelector, useDispatch} from "react-redux";
+import {increment, decrement} from "./actions/";
 
 function App() {
-    return <div className="App"></div>;
+    const counter = useSelector((state) => state.counter);
+    const login = useSelector((state) => state.login);
+    const dispatch = useDispatch();
+    return (
+        <div className="App">
+            <h1>Counter: {counter}</h1>
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <p>{login ? <h3>Logined</h3> : <h3>Not yet logined</h3>}</p>
+        </div>
+    );
 }
 
 export default App;
