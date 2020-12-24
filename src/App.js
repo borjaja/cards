a8760c8446b6583fff9b91f44de573186ad437b1
Borja Alvarez <balvarez@orienteed.com>
2020-12-24 11:18:24 +0100

    redux simplified

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 import React from "react";
 import "./App.css";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Post from "./components/Post";
-import PageNotFound from "./components/PageNotFound";
-import ProtectedRoute from "./components/ProtectedRoute";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Navbar />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <ProtectedRoute path="/contact" component={Contact} />
-                    <ProtectedRoute path="/about" component={About} />
-                    <ProtectedRoute path="/post/:post_id" component={Post} />
-                    <Route component={PageNotFound} />
-                </Switch>
-            </div>
-        </Router>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {createStore} from "redux";
+//actions
+const increment = (value = 1) => {
+    return {type: "INCREMENT", payload: value};
+};
+const decrement = (value = 1) => {
+    return {type: "DECREMENT", payload: value};
+};
 
+//reducers
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case increment().type:
+            return state + action.payload;
+        case decrement().type:
+            return state - action.payload;
+        default:
+            return state;
+    }
+};
+//Create store
+let store = createStore(counterReducer);
+//Display
+store.subscribe(() => {
+    console.log(store.getState());
+});
+//Dispatch the actions
+store.dispatch(increment(2));
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
